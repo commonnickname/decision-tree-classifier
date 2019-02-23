9092d0beb428fac0ddc27f5b2b7164883e219247
commonnickname <i.televan@student.tugraz.at>
2019-02-23 19:47:31 +0100

    create bsolver.py
    Solver for simple equations with bitwise operations

---
+++ bsolver.py
@@ -0,0 +1,30 @@
+from itertools import product as product
+
+OrTable = ((	(0,),	(1,)	),
+			(	(),		(0,1)	))
+		
+AndTable = ((	(0,1),	()		),
+			(	(0,),	(1,)	))
+
+def bits(num, length):
+	return [(num >> i) & 1 for i in range(length - 1, -1, -1)]
+	
+def bits_to_int(bits):
+	out = 0
+	for bit in bits: 
+		out = (out << 1) | bit
+	return out
+
+#generates all x < 2^l, such that num | x == res
+def orSolver(num, res, l):
+	L = (OrTable[n][r] for n, r in zip(bits(num, l), bits(res, l)))
+	for m in product(*L): yield bits_to_int(m)
+	
+#generates all x < 2^l, such that num & x == res
+def andSolver(num, res, l):
+	L = (AndTable[n][r] for n, r in zip(bits(num, l), bits(res, l)))
+	for m in product(*L): yield bits_to_int(m)
+
+
+
+	
